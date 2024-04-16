55a41f215ff055124697196ba3b895bd29c346cb
Clement Deville <cdeville@paul-f3Br4s6.clusters.42paris.fr>
2024-04-16 10:32:53 +0200

    Folder structure and Makefile

---
+++ src/mandatory/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 10:28:30 by cdeville          #+#    #+#             */
+/*   Updated: 2024/04/16 10:31:41 by cdeville         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philosophers.h>
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	ft_printf("Hello philo!\n");
+	return (0);
+}
